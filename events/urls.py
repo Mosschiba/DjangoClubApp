0bec9eaaf1e756e320f6e9b5f7e236451ce8486f
unknown <mousslemchibane@gmail.com>
2023-11-22 21:13:51 +0400

    add link to download txt, csv and pdf file

--- events/urls.py
+++ events/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path('add_event', views.add_event, name="add-event"),
     path('update_event/<event_id>', views.update_event, name="update-event"),
     path('delete_event/<event_id>', views.delete_event, name="delete-event"),
+    path('venue_text/', views.venue_text, name="venue_text"),
+    path('venue_csv/', views.venue_csv, name="venue_csv"),
+    path('venue_pdf/', views.venue_pdf, name="venue_pdf"),
 ]
--- events/views.py
+++ events/views.py
@@ -5,6 +5,85 @@ from datetime import datetime
 from .models import Venue, Event
 from .forms import VenueForm, EventForm
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+import csv
+
+# pdf things to import 
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+# Generate PDF file
+def venue_pdf(request):
+    # create Buffer
+    buf= io.BytesIO()
+    # create a canvas
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    # create text object
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont('Helvetica', 14)
+    # Designate the Model
+    venues = Venue.objects.all()
+    lines = []
+
+    for venue in venues:
+        lines.append(venue.name)
+        lines.append(venue.address)
+        lines.append(venue.zip_code)
+        lines.append(venue.phone)
+        lines.append(venue.web)
+        lines.append(venue.email_address)
+        lines.append("  ")
+    # loop throught the lines
+    for line in lines:
+        textob.textLine(line)
+    
+    # finich the reportlab thing
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+    # return the something here is will be response
+    return FileResponse(buf, as_attachment=True, filename='venue.pdf')
+
+    
+
+# Generate CSV pile (EXCEL)
+def venue_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-disposition'] = 'attachment; filename = venues.csv'
+
+    # create a csv writer this comes with the import csv
+    writer = csv.writer(response)
+    # Designate the models to pprint
+    venues = Venue.objects.all()
+
+    # add culomn heading to the csv file
+    writer.writerow(['Venue Name', 'Address', 'Zip Code', 'Phone', 'Website', 'Email'])
+   
+    for venue in venues:
+        writer.writerow([venue.name, venue.address, venue.zip_code, venue.phone, venue.web, venue.email_address])
+
+    return response
+    
+
+
+# Generate text file dynamicly
+def venue_text(request):
+    response = HttpResponse(content_type='text/plain')
+    response['Content-disposition'] = 'attachment; filename = venues.txt'
+    # Designate the models to pprint
+    venues = Venue.objects.all()
+    lines = []
+    for venue in venues:
+        lines.append(f'{venue.name}\n{venue.address}\n{venue.zip_code}\n{venue.phone}\n{venue.web}\n{venue.email_address}\n\n\n\n\n')
+
+    response.writelines(lines)
+    return response
 
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
